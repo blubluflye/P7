d18295be1ae2afaf54ed449e84223344b7182a82
Sleepy Grizzly <nicolas.bessonnet96@orange.com>
2023-08-24 12:36:53 +0200

    forgot @Configuration in securrityConfig.java

--- poseidonInc/src/main/java/com/openclassrooms/poseidonInc/config/SecurityConfig.java
+++ poseidonInc/src/main/java/com/openclassrooms/poseidonInc/config/SecurityConfig.java
@@ -2,14 +2,18 @@ package com.openclassrooms.poseidonInc.config;
 
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 	
 	@Bean
