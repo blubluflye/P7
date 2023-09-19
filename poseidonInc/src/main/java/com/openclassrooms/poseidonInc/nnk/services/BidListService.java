3dfbd0a152e2c1731ac35a61fbf9fba9c20efe99
Sleepy Grizzly <nicolas.bessonnet96@orange.com>
2023-09-19 17:32:16 +0200

    forgotten files in last update

---
+++ poseidonInc/src/main/java/com/openclassrooms/poseidonInc/nnk/services/BidListService.java
@@ -0,0 +1,43 @@
+package com.openclassrooms.poseidonInc.nnk.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.openclassrooms.poseidonInc.nnk.domain.BidList;
+import com.openclassrooms.poseidonInc.nnk.repositories.BidListRepository;
+
+@Service
+public class BidListService {
+
+	@Autowired
+	BidListRepository bidListRepository;
+	
+	public List<BidList> findAll() {
+		return bidListRepository.findAll();
+	}
+	
+	public BidList createBid(BidList bidList) {
+		return bidListRepository.save(bidList);
+	}
+	
+	public BidList readBid(Integer id) {
+		Optional<BidList> result = bidListRepository.findById(id);
+		if (result.isPresent())
+			return result.get();
+		return null;
+	}
+	
+	public BidList updateBid(BidList bidList) {
+		if (bidListRepository.findById(bidList.getBidListId()).isPresent())
+			return bidListRepository.save(bidList);
+		return null;
+	}
+	
+	public void deleteBid(BidList bidList) {
+		if (bidListRepository.findById(bidList.getBidListId()).isPresent())
+			bidListRepository.delete(bidList);
+	}
+}
