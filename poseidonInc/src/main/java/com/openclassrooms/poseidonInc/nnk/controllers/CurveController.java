414d751816d591e24e0686f044eb3cd1de5a9e2f
Sleepy Grizzly <nicolas.bessonnet96@orange.com>
2023-09-21 10:00:02 +0200

    CurvePoint service and controller added and working

--- poseidonInc/src/main/java/com/openclassrooms/poseidonInc/nnk/controllers/CurveController.java
+++ poseidonInc/src/main/java/com/openclassrooms/poseidonInc/nnk/controllers/CurveController.java
@@ -1,5 +1,6 @@
 package com.openclassrooms.poseidonInc.nnk.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -9,17 +10,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.openclassrooms.poseidonInc.nnk.domain.CurvePoint;
+import com.openclassrooms.poseidonInc.nnk.services.CurvePointService;
 
 import jakarta.validation.Valid;
 
 @Controller
 public class CurveController {
     // TODO: Inject Curve Point service
-
+	@Autowired
+	CurvePointService curvePointService;
+	
     @RequestMapping("/curvePoint/list")
     public String home(Model model)
     {
-        // TODO: find all Curve Point, add to model
+    	model.addAttribute("curvePoints",curvePointService.findAll());
         return "curvePoint/list";
     }
 
@@ -31,12 +35,19 @@ public class CurveController {
     @PostMapping("/curvePoint/validate")
     public String validate(@Valid CurvePoint curvePoint, BindingResult result, Model model) {
         // TODO: check data valid and save to db, after saving return Curve list
+    	if (!result.hasErrors()) {
+    		curvePointService.create(curvePoint);
+    		model.addAttribute("curvePoints",curvePointService.findAll());
+    		return "redirect:/curvePoint/list";
+    	}
+    	
         return "curvePoint/add";
     }
 
     @GetMapping("/curvePoint/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        // TODO: get CurvePoint by Id and to model then show to the form
+    	CurvePoint curvepoint =  curvePointService.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid CurvePoint Id:" + id));
+    	model.addAttribute("curvePoint", curvepoint);
         return "curvePoint/update";
     }
 
@@ -44,12 +55,16 @@ public class CurveController {
     public String updateBid(@PathVariable("id") Integer id, @Valid CurvePoint curvePoint,
                              BindingResult result, Model model) {
         // TODO: check required fields, if valid call service to update Curve and return Curve list
+    	curvePointService.update(curvePoint);
+		model.addAttribute("curvePoints",curvePointService.findAll());
         return "redirect:/curvePoint/list";
     }
 
     @GetMapping("/curvePoint/delete/{id}")
     public String deleteBid(@PathVariable("id") Integer id, Model model) {
-        // TODO: Find Curve by Id and delete the Curve, return to Curve list
+    	curvePointService.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid CurvePoint Id:" + id));
+    	curvePointService.delete(id);
+		model.addAttribute("curvePoints",curvePointService.findAll());
         return "redirect:/curvePoint/list";
     }
 }
---
+++ poseidonInc/src/main/java/com/openclassrooms/poseidonInc/nnk/services/CurvePointService.java
@@ -0,0 +1,40 @@
+package com.openclassrooms.poseidonInc.nnk.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.openclassrooms.poseidonInc.nnk.domain.CurvePoint;
+import com.openclassrooms.poseidonInc.nnk.repositories.CurvePointRepository;
+
+import jakarta.validation.Valid;
+
+@Service
+public class CurvePointService {
+	
+	@Autowired
+	CurvePointRepository curvePointRepository;
+
+	public  List<CurvePoint> findAll() {
+		return curvePointRepository.findAll();
+	}
+
+	public CurvePoint create(@Valid CurvePoint curvePoint) {
+		return curvePointRepository.save(curvePoint);
+	}
+
+	public  Optional<CurvePoint> findById(Integer id) {
+		return curvePointRepository.findById(id);
+	}
+
+	public CurvePoint update(@Valid CurvePoint curvePoint) {
+		return curvePointRepository.save(curvePoint);
+	}
+
+	public void delete(Integer id) {
+		curvePointRepository.deleteById(id);
+	}
+
+}
